597d35ad317904c6ea5b7b778cf77eac5a66e10c
Shrut Sujit Jaiswal <101062163+Batman150651@users.noreply.github.com>
2025-08-27 20:49:15 +0530

    Time: 2 ms (21.24%) | Memory: 8.6 MB (14.62%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<2) return x;
+        int l = 1,h = x/2;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (mid >  (x/mid)) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return h;
+    }
+};
