d8ddb94aebf862b87a4b85920edf36cb8362d0c1
Shrut Sujit Jaiswal <101062163+Batman150651@users.noreply.github.com>
2025-08-31 13:55:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (32.67%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,73 +1,21 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        string res = "";
-        while(num!=0){
-            if(num>=1000){
-                res+='M';
-                num-=1000;
-            }
-            else if(num>=500){
-                if(num/100==9){
-                    res+="CM";
-                    num-=900;
-                }
-                else{
-                    res+="D";
-                    num-=500;
-                }
-            }
-            else if(num>=100){
-                if(num/100==4){
-                    res+="CD";
-                    num-=400;
-                }
-                else{
-                    res+='C';
-                    num-=100;
-                }
-            }
-            else if(num>=50){
-                if(num/10==9){
-                    res+="XC";
-                    num-=90;
-                }
-                else{
-                    res+='L';
-                    num-=50;
-                }
-            }
-            else if(num>=10){
-                if(num/10==4){
-                    res+="XL";
-                    num-=40;
-                }
-                else{
-                    res+='X';
-                    num-=10;
-                }
-            }
-            else if(num>=5){
-                if(num==9){
-                    res+="IX";
-                    num-=9;
-                }
-                else{
-                    res+='V';
-                    num-=5;
-                }
-            }
-            else if(num>=1){
-                if(num==4){
-                    res+="IV";
-                    num-=4;
-                }
-                else{
-                    res+='I';
-                    num-=1;
-                };
+        vector<pair<int, string>> valueSymbols = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+        };
+        string result = "";
+        for (auto& pair : valueSymbols) {
+            int value = pair.first;
+            string symbol = pair.second;
+            while (num >= value) {
+                result += symbol;
+                num -= value;
             }
         }
-        return res;
+        
+        return result;
     }
 };
